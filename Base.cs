7b7f8296aa227c289834b533c8999ddb5e4fb8f6
JakubH <homarofficial@gmail.com>
2022-12-25 16:31:44 +0100

    Fin

--- Base.cs
+++ Base.cs
@@ -5,8 +5,24 @@ namespace Czas24hWDP1
     {
         public static void Main()
         {
-            Czas24h Time = new Czas24h(2, 4, 6);
-            Console.WriteLine(Time);
+            // Test 1
+            var t = new Czas24h(2, 15, 37);
+            t.Minuta = 20;
+            t.Godzina = 1;
+            t.Sekunda = 26;
+            Console.WriteLine(t);
+
+            // Test 4
+            t = new Czas24h(2, 15, 37);
+            t.Minuta = 20;
+            t.Godzina = 23;
+            t.Godzina = 1;
+            t.Sekunda = 15;
+            t.Minuta = 10;
+            t.Sekunda = 23;
+            t.Sekunda = 1;
+            t.Minuta = 12;
+            Console.WriteLine(t);
         }
     }
 }
--- Czas24h.cs
+++ Czas24h.cs
@@ -8,25 +8,40 @@ namespace Czas24hWDP1
         public int Sekunda
         {
             get => liczbaSekund - Godzina * 60 * 60 - Minuta * 60;
-            // uzupełnij kod - zdefiniuj setters'a
+            set
+            {
+                if (value < 0 || value > 59)
+                    throw new ArgumentException("error");
+                liczbaSekund = liczbaSekund - (liczbaSekund % 60) + value;
+            }
         }
 
         public int Minuta
         {
             get => (liczbaSekund / 60) % 60;
-            // uzupełnij kod - zdefiniuj setters'a
+            set
+            {
+                if (value < 0 || value > 59)
+                    throw new ArgumentException("error");
+                liczbaSekund = liczbaSekund - (liczbaSekund / 60 % 60 * 60) + (value * 60);
+            }
         }
 
         public int Godzina
         {
             get => liczbaSekund / 3600;
-            // uzupełnij kod - zdefiniuj setters'a
+            set
+            {
+                if (value < 0 || value > 23)
+                    throw new ArgumentException("error");
+                liczbaSekund = liczbaSekund - (liczbaSekund / 3600 * 3600) + (value * 3600);
+            }
         }
 
         public Czas24h(int godzina, int minuta, int sekunda)
         {
-            // uzupełnij kod zgłaszając wyjątek ArgumentException
-            // w sytuacji niepoprawnych danych
+            if (godzina < 0 || godzina > 23 || minuta < 0 || minuta > 59 || sekunda < 0 || sekunda > 59)
+                throw new ArgumentException("error");
 
             liczbaSekund = sekunda + 60 * minuta + 3600 * godzina;
         }
